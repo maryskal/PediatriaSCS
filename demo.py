7cbe703bfec907522d4b0e43e43a2841f641bd29
María Rollán <mrollan@vicomtech.org>
2021-03-03 23:18:43 +0100

    Numeros y si y no

--- demo.py
+++ demo.py
@@ -50,15 +50,24 @@ protocolo_json = {
 
 formularios = {
     'fiebre':{
-        'Tiempo de fiebre': ['1d', '2d'],
-        'Temperatura maxima': ['38','39','40'],
-        'Mocos y tos':['si', 'no'],
-        'Vomitos':['si', 'no']
+        'bool':{
+        'Mocos y tos': ['si', 'no'],
+        'Vomitos': ['si', 'no']
+        },
+
+        'num':{
+        'Tiempo de fiebre (horas)': [1,120],
+        'Temperatura maxima (ºC)': [37.0,42.0]
+        }
+
     }
 }
+
+
 def funcion_protocolo(respuestas = [], nodo_name= "start", protocolo_json=protocolo_json):
     nodo = protocolo_json.get(nodo_name)
     tipo = nodo.get('tipo')
+
     if tipo == "pregunta":
         respuestas = respuestas + [st.selectbox(
             nodo.get("pregunta"),
@@ -76,15 +85,35 @@ def funcion_protocolo(respuestas = [], nodo_name= "start", protocolo_json=protoc
         st.write('Con esta informacion se recomienda que')
 
     elif tipo == 'formulario':
-        form = formularios.get(nodo_name)
+        sintoma_guia = formularios.get(nodo_name)
         rspss = list()
         preg = list()
-        for pregunta in form.keys():
-            preg.append(pregunta)
-            rspss.append(st.selectbox(
-                pregunta,
-                ['none'] + list(form.get(pregunta))
-            ))
+
+        #Según si es bool o numérico se hace una cosa u otra
+        for tipo_pregunta in sintoma_guia.keys():
+
+            #Recogemos el tipo de pregunta
+            tipo = sintoma_guia.get(tipo_pregunta)
+
+            #Si tipo bool
+            if(tipo_pregunta == 'bool'):
+                #Recorro todas las preguntas y recojo las respuestas
+                for pregunta in tipo.keys():
+                    preg.append(pregunta)
+                    rspss.append(st.selectbox(
+                        pregunta,
+                        ['none'] + list(tipo.get(pregunta))
+                    ))
+            #Si es tipo numerico
+            elif (tipo_pregunta == 'num'):
+                # Recorro todas las preguntas
+                for pregunta in tipo.keys():
+                    #Recogemos valores máximo y minimo para el rango
+                    max_min= list(tipo.get(pregunta))
+                    preg.append(pregunta)
+                    rspss.append(st.slider(
+                        pregunta, max_min[0], max_min[1]))
+
 
         resultados = pd.DataFrame(rspss, preg)
         st.write(resultados)
